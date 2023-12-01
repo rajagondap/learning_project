20c8a290a6515e8017fe8d1cf162836d5d7efa9f
rajagondap <rajap786@gmail.com>
2023-12-01 16:54:41 +0530

    Added the logic and pages for login, registration forms.py, settings, urls, changes in the models for login and registration

--- Blog/articles/views.py
+++ Blog/articles/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import Article
+from .forms import LoginForm, UserRegistration
+from django.contrib.auth import authenticate, login
 
 
 # Create your views here.
@@ -7,3 +9,42 @@ from .models import Article
 def article_list(request):
     article_list = Article.objects.all().order_by('-published')
     return render(request, 'articles.html', {'article_list': article_list})
+
+
+def article_details(request, slug):
+    article = get_object_or_404(Article, slug=slug)
+    return render(request, 'article_details.html', {'article': article})
+
+
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+
+            if user is not None:
+                login(request, user)
+                return HttpResponse(" You are authenticated")
+            else:
+                return HttpResponse("Invalid Login")
+    else:
+        form = LoginForm()
+
+    return render(request, 'account/login.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistration(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+
+            new_user.save()
+            return render(request, 'account/register_done.html', {'user_form': user_form})
+
+    else:
+        user_form = UserRegistration()
+
+    return render(request, 'account/register.html', {'user_form': user_form})
