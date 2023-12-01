f53d892b7e80a978a13118cca0d4701c1130eab5
rajagondap <rajap786@gmail.com>
2023-12-01 21:08:37 +0530

    Added the logic and pages for login, registration forms.py, settings, urls, changes in the models for login and registration

--- Blog/articles/urls.py
+++ Blog/articles/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path
 from .views import article_list, article_details, user_login, register
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', article_list, name='article_list'),
     path('articles/<slug:slug>/', article_details, name='article_details'),
-    path('login/', user_login, name='login'),
+    # path('login/', user_login, name='login'),
+    path('login/', LoginView.as_view(), name='login'),
     path('register/', register, name='register'),
 ]
